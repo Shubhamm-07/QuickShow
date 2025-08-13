4f879e6c3cfca8f8f46f4e6c66dcb99955c5b928
Shubham Gupta <shubhammkumar52@gmail.com>
2025-08-13 14:28:28 +0530

    debugging

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -9,6 +9,7 @@ const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
     { event: 'clerk/user.created'},
     async ({event}) =>{
+        console.log(event)
         console.log("✅ Clerk event received:", event.data);
         const {id, first_name, last_name, email_addresses, image_url} = event.data
         const userData = {
