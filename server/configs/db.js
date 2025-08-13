3591cd4d9ad4f271858b3a992c53698a05f3769d
Shubham Gupta <shubhammkumar52@gmail.com>
2025-08-13 11:47:35 +0530

    undo db changes

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,28 +1,12 @@
 import mongoose from 'mongoose';
 
-let isConnected = false; // Track connection status
-
-const connectDB = async () => {
-  if (isConnected) {
-    // If already connected, reuse the connection
-    return;
-  }
-
-  try {
-    const conn = await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    isConnected = conn.connections[0].readyState === 1;
-
-    if (isConnected) {
-      console.log('✅ MongoDB connected');
+const connectDB = async () =>{
+    try{
+         mongoose.connection.on('connected', ()=> console.log('Database connected'));
+        await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
+    } catch(error){
+        console.log(error.message)
     }
-  } catch (error) {
-    console.error('❌ MongoDB connection error:', error.message);
-    throw error;
-  }
-};
+}
 
 export default connectDB;
--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,4 @@
 import {Inngest} from 'inngest';
-import connectDB from '../configs/db.js';
 import User from '../models/User.js';
 
 // Create a client to send and receive events
@@ -9,9 +8,7 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
     { event: 'clerk/user.created'},
-
     async ({event}) =>{
-        await connectDB(); 
         const {id, first_name, last_name, email_addresses, image_url} = event.data
         const userData = {
             _id: id,
@@ -26,9 +23,7 @@ const syncUserCreation = inngest.createFunction(
 const syncUserDeletion = inngest.createFunction(
     {id: 'delete-user-with-clerk'},
     { event: 'clerk/user.deleted'},
-    
     async ({event}) =>{
-        await connectDB(); 
         
         const {id} = event.data
         await User.findByIdAndDelete(id)
@@ -39,9 +34,7 @@ const syncUserDeletion = inngest.createFunction(
 const syncUserUpdation = inngest.createFunction(
     {id: 'update-user-from-clerk'},
     { event: 'clerk/user.updated'},
-
     async ({event}) =>{
-        await connectDB(); 
         
         const {id, first_name, last_name, email_addresses, image_url} = event.data
          const userData = {
